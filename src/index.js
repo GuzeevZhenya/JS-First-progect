dcaab87490a5d05b4be380d2f18d4012fb2b93b1
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-10-19 23:27:57 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import { initForm } from "./form/form";
+// import { chipsCreate, deleteChips, addChips } from "./chips.js";
+// import { form, btn, INVALID_CLASS, users, formHelper, handleClassAdding } from "./register";
+// import { bigestBalance, calculateUsers, usersInfo } from "./users-table";
+// import { login, password, btnLogin, formLogin, div, setDisabledStatus, empty, } from "./login.js";
+
+import {initForm} from "./form/form";
+import {initRouter} from './form/router';
+import {initUsers} from "./form/users-table";
+
+
 initForm();
+initRouter();
