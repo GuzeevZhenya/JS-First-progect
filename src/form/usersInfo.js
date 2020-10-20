ddd525ccd8a9706e6bf644371885fe73bfb2200d
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-10-20 12:14:59 +0300

    add

--- src/form/usersInfo.js
+++ src/form/usersInfo.js
@@ -1,61 +1,72 @@
+import { Template } from "webpack";
+
 export const usersInfo = [{
-    name: "Alex",
-    company: "Google",
-    email: "alex @gmail.com",
-    phone: "+375292929292",
-    balance: "$54",
-    isActive: false,
-    gender: "female",
-},
-{
-    name: "Vlad",
-    company: "Yandex",
-    email: "vlad @gmail.com",
-    phone: "+375293429232",
-    balance: "$64",
-    isActive: false,
-    gender: "female",
-}, {
-    name: "Igor",
-    company: "Google",
-    email: "alex @gmail.com",
-    phone: "+37529223492",
-    balance: "$57",
-    isActive: false,
-    gender: "female",
-}, {
-    name: "Stas",
-    company: "Google",
-    email: "alex @gmail.com",
-    phone: "+375292929292",
-    balance: "$79",
-    isActive: false,
-    gender: "male",
-}, {
-    name: "Sergei",
-    company: "Google",
-    email: "alex @gmail.com",
-    phone: "+375292929292",
-    balance: "$43",
-    isActive: false,
-    gender: "male",
-}, {
-    name: "Viktor",
-    company: "Google",
-    email: "alex @gmail.com",
-    phone: "+375292929292",
-    balance: "$54",
-    isActive: false,
-    gender: "male",
-}, {
-    name: "Zhenya",
-    company: "Google",
-    email: "alex @gmail.com",
-    phone: "+375292929292",
-    balance: "$58",
-    isActive: false,
-    gender: "female",
-},
+        name: "Alex",
+        company: "Google",
+        email: "alex @gmail.com",
+        phone: "+375292929292",
+        balance: "$54",
+        isActive: false,
+        gender: "female",
+    },
+    {
+        name: "Vlad",
+        company: "Yandex",
+        email: "vlad @gmail.com",
+        phone: "+375293429232",
+        balance: "$64",
+        isActive: false,
+        gender: "female",
+    }, {
+        name: "Igor",
+        company: "Google",
+        email: "alex @gmail.com",
+        phone: "+37529223492",
+        balance: "$57",
+        isActive: false,
+        gender: "female",
+    }, {
+        name: "Stas",
+        company: "Google",
+        email: "alex @gmail.com",
+        phone: "+375292929292",
+        balance: "$79",
+        isActive: false,
+        gender: "male",
+    }, {
+        name: "Sergei",
+        company: "Google",
+        email: "alex @gmail.com",
+        phone: "+375292929292",
+        balance: "$43",
+        isActive: false,
+        gender: "male",
+    }, {
+        name: "Viktor",
+        company: "Google",
+        email: "alex @gmail.com",
+        phone: "+375292929292",
+        balance: "$54",
+        isActive: false,
+        gender: "male",
+    }, {
+        name: "Zhenya",
+        company: "Google",
+        email: "alex @gmail.com",
+        phone: "+375292929292",
+        balance: "$58",
+        isActive: false,
+        gender: "female",
+    },
 ];
 
 
+export const initClients = () => {
+    ROOT_NODE.innerHTML = template;
+    fetch("https://gist.githubusercontent.com/oDASCo/3f4014d24dc79e1e29b58bfa96afaa1b/raw/677516ee3bd278f7e3d805108596ca431d00b629/db.json").then((response) => {
+        response.json();
+    }).then(users => {
+        createUsersTab(users);
+    });
+
+};
--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@
 // import { bigestBalance, calculateUsers, usersInfo } from "./users-table";
 // import { login, password, btnLogin, formLogin, div, setDisabledStatus, empty, } from "./login.js";
 
-import {initForm} from "./form/form";
-import {initRouter} from './form/router';
-import {initUsers} from "./form/users-table";
+import { initForm } from "./form/form";
+import { initRouter } from './form/router';
+import { initTableOfUsers } from "./form/users-table";
 
 
-initForm();
-initRouter();
+// initForm();
+// initRouter();
+// initTableOfUsers();
+
+const checkIsEven = (number) => {
+    return newPromise((resolve, reject) => {
+        if (number % 2 === 0) {
+            resolve(number);
+        } else {
+            reject('It bad');
+        }
+    });
+}
+
+
+checkIsEven(9)
+    .then((number) => {
+        console.log("hey,it's good number", number);
+        return number + 2
+    })
+    .then((number) => {
+        return number + 3
+    })
+    .catch((error) => {
+        console.log("sorry", error);
+    });
