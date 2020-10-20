d5ce859b4a18191f6b8eb04e276b7b8f46605d51
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-10-20 16:41:17 +0300

    add

--- src/form/login.js
+++ src/form/login.js
@@ -1,64 +1,64 @@
 import { chipsCreate } from "./chips";
 
- export const initLogin = () => {
-     const login = document.querySelector('.login');
-     const password = document.querySelector('.password');
-     const btnLogin = document.querySelector('#btnLogin');
-     const formLogin = document.querySelector('form');
-     const div = document.querySelector('.div');
+export const initLogin = () => {
+    const login = document.querySelector('.login');
+    const password = document.querySelector('.password');
+    const btnLogin = document.querySelector('#btnLogin');
+    const formLogin = document.querySelector('form');
+    const div = document.querySelector('.div');
 
-     
 
-     login.addEventListener('input', (event) => {
-         if (document.body.contains(div)) {
-             div.remove();
-         }
-         empty(event);
-     });
 
-     password.addEventListener('input', (event) => {
-         empty(event);
-         if (document.body.contains(div)) {
-             div.remove();
-         }
-     });
+    login.addEventListener('input', (event) => {
+        if (document.body.contains(div)) {
+            div.remove();
+        }
+        empty(event);
+    });
 
-     //Проверка чтобы 2 поля были заполнены
-     function setDisabledStatus() {
-         if (login.value === "" || password.value === "") {
-             btnLogin.disabled = true;
-         } else {
-             btnLogin.disabled = false;
-         }
-     }
+    password.addEventListener('input', (event) => {
+        empty(event);
+        if (document.body.contains(div)) {
+            div.remove();
+        }
+    });
 
-     function empty(event) {
-         const isValid = event.target.value;
-         if (isValid === "") {
-             btnLogin.disabled = true;
-             event.target.classList.add('invalid');
-         } else {
-             btnLogin.disabled = false;
-             event.target.classList.remove('invalid');
-         }
-         setDisabledStatus();
-     }
-     
-     btnLogin.addEventListener('click', (event) => {
-         event.preventDefault();
-         if (login.value == users.email && password.value == users.password) {
-             let div = document.createElement('div');
-             document.body.appendChild(div);
-             chipsCreate('Добро пожаловать');
-             form.remove();
-         } else {
-             let div = document.createElement('div');
-             div.classList.add('div');
-             document.body.appendChild(div);
-             chipsCreate('Ошибка, повторите ввод данных');
-             password.value = "";
-             login.value = "";
-             btnLogin.disabled = true;
-         }
-     })
- }
+    //Проверка чтобы 2 поля были заполнены
+    function setDisabledStatus() {
+        if (login.value === "" || password.value === "") {
+            btnLogin.disabled = true;
+        } else {
+            btnLogin.disabled = false;
+        }
+    }
+
+    function empty(event) {
+        const isValid = event.target.value;
+        if (isValid === "") {
+            btnLogin.disabled = true;
+            event.target.classList.add('invalid');
+        } else {
+            btnLogin.disabled = false;
+            event.target.classList.remove('invalid');
+        }
+        setDisabledStatus();
+    }
+
+    btnLogin.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (login.value == users.email && password.value == users.password) {
+            let div = document.createElement('div');
+            document.body.appendChild(div);
+            chipsCreate('Добро пожаловать');
+            form.remove();
+        } else {
+            let div = document.createElement('div');
+            div.classList.add('div');
+            document.body.appendChild(div);
+            chipsCreate('Ошибка, повторите ввод данных');
+            password.value = "";
+            login.value = "";
+            btnLogin.disabled = true;
+        }
+    })
+}
--- src/index.js
+++ src/index.js
@@ -8,29 +8,6 @@ import { initRouter } from './form/router';
 import { initTableOfUsers } from "./form/users-table";
 
 
-// initForm();
-// initRouter();
-// initTableOfUsers();
-
-const checkIsEven = (number) => {
-    return newPromise((resolve, reject) => {
-        if (number % 2 === 0) {
-            resolve(number);
-        } else {
-            reject('It bad');
-        }
-    });
-}
-
-
-checkIsEven(9)
-    .then((number) => {
-        console.log("hey,it's good number", number);
-        return number + 2
-    })
-    .then((number) => {
-        return number + 3
-    })
-    .catch((error) => {
-        console.log("sorry", error);
-    });
+initForm();
+initRouter();
+initTableOfUsers();
